3f378783100ada6fff4e9f108fa0e415b82477e8
tktk <tktk.stereoman@gmail.com>
2016-05-17 23:53:44 +0900

    fix in onTick

--- mezamashi.js
+++ mezamashi.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 // IRKitServiceまわり
 var endpoint = 'http://192.168.3.102:8080/IRKitService/api/send/';
-var eight = endpoint + 'tv_eight';
+var eight = endpoint + 'tv_eight'; // 8ch
 var blue = endpoint + 'tv_blue';
 var red = endpoint + 'tv_red';
 var green = endpoint + 'tv_green';
@@ -12,44 +12,40 @@ var yellow = endpoint + 'tv_yellow';
 
 // 5:57
 first = new CronJob({
-    cronTime: '*/10 57 5 * * *', // 5:57に10秒ごと
+    cronTime: '*/10 57 5 * * 1-5', // 5:57に10秒ごと
     start: true,
     timeZone: 'Asia/Tokyo',
     onTick: function(){
-        req(endpoint + 'tv_pow');
+        req(endpoint + 'tv_pow'); //tv on
         req(eight);
-        var hand = Math.floor( Math.random() * 3 ); // 0~2の乱数
-        jyanken(hand);
+        jyanken(Math.floor( Math.random() * 3 )); // 0~2の乱数
     }
 });
 // 6:57
-second= new CronJob({
-    cronTime: '*/10 57 6 * * *', // 6:57に10秒ごと
+second = new CronJob({
+    cronTime: '*/10 57 6 * * 1-5', // 6:57に10秒ごと
     start: true,
     timeZone: 'Asia/Tokyo',
     onTick: function(){
-        var hand = Math.floor( Math.random() * 3 ); // 0~2の乱数
-        jyanken(hand);
+        jyanken(Math.floor( Math.random() * 3 )); // 0~2の乱数
     }
 });
 // 7:24
 third = new CronJob({
-    cronTime: '*/10 24 7 * * *', // 7:24に10秒ごと
+    cronTime: '*/10 24 7 * * 1-5', // 7:24に10秒ごと
     start: true,
     timeZone: 'Asia/Tokyo',
     onTick: function(){
-        var hand = Math.floor( Math.random() * 3 ); // 0~2の乱数
-        jyanken(hand);
+        jyanken(Math.floor( Math.random() * 3 )); // 0~2の乱数
     }
 });
 // 7:57
 fourth = new CronJob({
-    cronTime: '*/10 57 7 * * *', // 7:57に10秒ごと
+    cronTime: '*/10 57 7 * * 1-5', // 7:57に10秒ごと
     start: true,
     timeZone: 'Asia/Tokyo',
     onTick: function(){
-        var hand = Math.floor( Math.random() * 3 ); // 0~2の乱数
-        jyanken(hand);
+        jyanken(Math.floor( Math.random() * 3 )); // 0~2の乱数
     }
 });
 
@@ -64,6 +60,9 @@ function jyanken(hand){
     case 2:
         req(green);
         break;
+    default:
+        req(red);
+        break;
     }
 }
 
@@ -72,6 +71,9 @@ function req(url){
     request(url, function(err, res, body){
         if(!err && res.statusCode == 200){
             console.log(url);
+        }
+        else{
+            console.log(err);
         }
     });
 }
